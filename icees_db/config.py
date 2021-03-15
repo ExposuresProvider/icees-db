531916fdc4a77d1ea5a24033ffc28a8e375b6ecb
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-03-15 09:55:30 -0400

    Fix default config path

--- icees_db/config.py
+++ icees_db/config.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 
 def get_config_path():
-    return os.environ.get("CONFIG_PATH", Path(__file__).parent.parent.parent / "config")
+    return os.environ.get("CONFIG_PATH", Path(__file__).parent.parent / "config")
